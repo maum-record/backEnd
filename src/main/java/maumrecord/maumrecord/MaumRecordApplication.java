8778b52fcb3bed7df74cc8f511039048a6f47093
JIN <126974009+Jin4041@users.noreply.github.com>
2025-03-24 16:23:50 +0900

    init

---
+++ src/main/java/maumrecord/maumrecord/MaumRecordApplication.java
@@ -0,0 +1,13 @@
+package maumrecord.maumrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaumRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaumRecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/maumrecord/maumrecord/MaumRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package maumrecord.maumrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaumRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
